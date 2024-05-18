142f5fb21023d8615f756f7df84de1e36ddf668f
hackerAman <104302829+hackerAman1729@users.noreply.github.com>
2024-05-18 10:13:25 +0000

    Neetcode roadmap q1

--- .ccls-cache/@home@runner@CodeforcesCP/945Div2@q1.cpp
+++ .ccls-cache/@home@runner@CodeforcesCP/945Div2@q1.cpp
@@ -1,34 +1,20 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
-
-int max_draws(int p1, int p2, int p3) {
-    if ((p1 + p2 + p3) % 2 != 0) {
-        return -1;
-    }
-
-    int totalGame = (p1 + p2 + p3) / 2;
-    int MaxDraws = min(totalGame, p1 + p2);
-
-    return MaxDraws;
-}
 
 int main() {
     int t;
     cin >> t;  
-    vector<int> results;
-
-    for (int i = 0; i < t; ++i) {
+    while(t--){
         int p1, p2, p3;
         cin >> p1 >> p2 >> p3;
-        results.push_back(max_draws(p1, p2, p3));
+      if((p1+p2+p3)%2 != 0){
+        cout<< -1 << endl ;
+        continue;
+      }
+  int totalGame = (p1 + p2 + p3) / 2;
+  
+  cout << min(totalGame, p1 + p2) << endl;
     }
-
-    for (int result : results) {
-        cout << result << endl;
-    }
-
     return 0;
 }
---
+++ Neetcode/q1.cpp
@@ -0,0 +1,25 @@
+// NeetCode roadmap question. https://leetcode.com/problems/contains-duplicate/description/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> v;
+    int num;
+
+    while (cin >> num) {
+        v.push_back(num);
+    }
+
+    unordered_map<int, int> freq;
+    for (int i : v) {
+        freq[i]++;
+        if (freq[i] > 1) {
+            cout << "true" << endl;
+            return 0;
+        }
+    }
+
+    cout << "false" << endl;
+    return 0;
+}
