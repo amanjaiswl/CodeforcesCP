e3c4f5b741f0e2c9b81652e58859c0125c9a13cf
hackerAman <104302829+hackerAman1729@users.noreply.github.com>
2024-05-26 09:00:51 +0000

    947Div12 q1

--- .ccls-cache/@home@runner@CodeforcesCP/947Div12@q1.cpp
+++ .ccls-cache/@home@runner@CodeforcesCP/947Div12@q1.cpp
@@ -1,52 +1,85 @@
+//  this is ques 2
+// so could not do it in the contest. I was being naive and checking both. but let's say that the sorted seq is 2,4,7,8,10. so it would n't work.
+
+/* this is my original submission which failed pretest 2
 #include <iostream>
-#include <vector>
 #include <algorithm>
-#include <numeric> // for std::gcd
+#include <string>
+#include <cmath>
+#include <vector>
 
 using namespace std;
 
-void solve() {
-    int n;
-    cin >> n;
-    vector<long long> a(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> a[i];
+
+void solve(){
+  long long n;
+  cin >> n;
+  vector<long long> a(n);
+  for (long i = 0; i < n; ++i) {
+      cin >> a[i];
+  }
+  sort(a.begin(), a.end());
+
+  long long first = a[0];
+  if(n>1){
+  long long second = a[1];
+
+  for(long long i=0; i<n; i++){
+    if(  ((a[i]%first) != 0) &&  ((a[i]%second) != 0)  ){
+      cout << "no" << endl;
+      return;
     }
-
-    sort(a.begin(), a.end());
-
-    // Check if there exist two elements such that all elements are divisible by at least one of them
-    long long gcd_all = a[0];
-    for (int i = 1; i < n; ++i) {
-        gcd_all = gcd(gcd_all, a[i]);
-    }
-
-    bool isBeautiful = false;
-
-    for (int i = 0; i < n; ++i) {
-        long long gcd_with_a_i = gcd_all;
-        for (int j = 0; j < n; ++j) {
-            if (i != j) {
-                gcd_with_a_i = gcd(gcd_with_a_i, a[j]);
-            }
-        }
-        if (gcd_with_a_i == a[i] || gcd_with_a_i == gcd_all) {
-            isBeautiful = true;
-            break;
-        }
-    }
-
-    if (isBeautiful) {
-        cout << "YES" << endl;
-    } else {
-        cout << "NO" << endl;
-    }
+    
+  }}
+  cout << "yes" << endl;
 }
 
-int main() {
+int main(){
+  int t;
+  cin >> t;
+  while(t--){
+    solve();
+  }
+}
+*/
+
+//  The correct code
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
     int t;
-    cin >> t;
-    while (t--) {
-        solve();
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int flag=0;
+      int a[n];
+        for(int i=1;i<=n;i++){
+            cin>>a[i];
+            if(a[i]==1)flag=1;
+        }
+        if(flag)cout<<"Yes" << endl;
+        else{
+            sort(a+1,a+1+n);
+            vector<int> b;
+            for(int i=2;i<=n;i++){
+                if(a[i]%a[1])b.push_back(a[i]);
+            }
+            sort(b.begin(),b.end());
+            n = b.size();
+            for(int j=1;j<n;j++){
+                if(b[j]%b[0]){
+                    flag=1;
+                    break;
+                }
+            }
+            if(!flag)cout<<"Yes" << endl;
+            else cout<<"No"<< endl;
+        }
+
     }
 }
