523858dc67571d45e6b37489144616f889bd00db
hackerAman <104302829+hackerAman1729@users.noreply.github.com>
2024-05-29 15:16:21 +0000

    TLE q18

--- .ccls-cache/@home@runner@CodeforcesCP/TLE@q18.cpp
+++ .ccls-cache/@home@runner@CodeforcesCP/TLE@q18.cpp
@@ -1,2 +1,35 @@
-// https://codeforces.com/problemset/problem/1831/A
-// 
+//https://codeforces.com/problemset/problem/1829/B
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+  int t;
+  cin >> t;
+  while(t--){
+    int n;
+    cin >> n;
+    int arr[n];
+
+    int maxzero = 0;
+    int currlen = 0;
+    
+    for(int i=0; i<n;i++){
+      cin >> arr[i];
+      if(arr[i]==0){
+        currlen++;
+      }
+      else{
+        maxzero = max(currlen, maxzero);
+        currlen =0;
+      }
+    }
+
+    cout << max(maxzero,currlen) << endl;
+
+
+    
+  }
+}
