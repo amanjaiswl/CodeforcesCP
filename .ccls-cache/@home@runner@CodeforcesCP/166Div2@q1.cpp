078dac500f3af5d805969677648cd741f56f288e
hackerAman <104302829+hackerAman1729@users.noreply.github.com>
2024-05-30 16:28:43 +0000

    166 Div2 - solved q1

--- .ccls-cache/@home@runner@CodeforcesCP/166Div2@q1.cpp
+++ .ccls-cache/@home@runner@CodeforcesCP/166Div2@q1.cpp
@@ -1,17 +1,49 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int main(){
-  int t;
-  cin >> t;
-  while(t--){
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        string pass;
+        cin >> pass;
+        vector<char> digits;
+        vector<char> letters;
+        bool invalid = false;
 
+        for (int i = 0; i < n; i++) {
+            if (isdigit(pass[i])) {
+                digits.push_back(pass[i]);
+            } else if (islower(pass[i])) {
+                letters.push_back(pass[i]);
+            } else {
+                cout << "NO" << endl;
+                invalid = true;
+                break;
+            }
+        }
 
-    
-  }
+        if (invalid) continue;
 
+        if (!letters.empty() && !digits.empty()) {
+            int firstLetterIndex = pass.find_first_of(letters[0]);
+            int lastDigitIndex = pass.find_last_of(digits.back());
+            if (firstLetterIndex < lastDigitIndex) {
+                cout << "NO" << endl;
+                continue;
+            }
+        }
 
+        if (!is_sorted(digits.begin(), digits.end()) || !is_sorted(letters.begin(), letters.end())) {
+            cout << "NO" << endl;
+            continue;
+        }
 
+        cout << "YES" << endl;
+    }
 }
--- 166Div2/q2.cpp
+++ 166Div2/q2.cpp
@@ -1,17 +1,52 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <climits>
 
 using namespace std;
 
-int main(){
-  int t;
-  cin >> t;
-  while(t--){
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
 
+        vector<int> b(n + 1);
+        for (int i = 0; i < n + 1; i++) {
+            cin >> b[i];
+        }
 
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += abs(b[i] - a[i]);
+        }
 
-  }
+        if (find(a.begin(), a.end(), b[n]) != a.end()) {
+            cout << sum + 1 << endl;
+        } else {
+            int minDiff = INT_MAX;
+            int closestElement = a[0];  
+            for (int i = 0; i < n; i++) {
+                int diff = abs(b[n] - a[i]);
+                if (diff < minDiff) {
+                    minDiff = diff;
+                    closestElement = a[i];
+                }
+            }
 
-
-
+            if (closestElement > b[n]) {
+                cout << sum + 1 << endl;
+            } else {
+                
+                int adjustment =  (b[n] - closestElement);
+                cout << sum + 1 + adjustment << endl;
+            }
+        }
+    }
+    return 0;
 }
