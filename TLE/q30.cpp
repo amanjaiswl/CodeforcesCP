ca83b1510c357d2a68cbb9f0db9e50cc46fd1e64
hackerAman <104302829+hackerAman1729@users.noreply.github.com>
2024-06-18 17:05:49 +0000

    TLE q30

--- TLE/q30.cpp
+++ TLE/q30.cpp
@@ -1,26 +1,31 @@
+// https://codeforces.com/problemset/problem/1679/A
 #include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
+#include <cmath>
 
 using namespace std;
 
 #define ll long long
 
 void solve(){
+    ll n;
+    cin >> n;
 
-  
+    if (n %2 != 0 || n < 4) {
+        cout << -1 << "\n";
+        return;
+    }
+
+    ll minBus = (n + 5) / 6; 
+    ll maxBus = n / 4;       
+
+    cout << minBus << " " << maxBus << "\n";
 }
 
-
-
-
 int main(){
-  int t;
-  cin>>t;
-  while (t--){
-    solve();
-
-  }
-  return 0;
+    int t;
+    cin >> t;
+    while (t--){
+        solve();
+    }
+    return 0;
 }
