956bca287be9da9671162d74d14e7ef29e50e4c0
hackerAman <104302829+hackerAman1729@users.noreply.github.com>
2024-06-19 09:00:20 +0000

    TLE q32

--- .ccls-cache/@home@runner@CodeforcesCP/TLE@q32.cpp
+++ .ccls-cache/@home@runner@CodeforcesCP/TLE@q32.cpp
@@ -8,12 +8,29 @@ using namespace std;
 #define ll long long
 
 void solve(){
-
-  
+  ll x,n;
+  cin>> x>>n;
+  if(n%4 == 1){
+    n= -n;
+  }
+  else if(n%4 == 2){
+    n = 1;
+  }
+  else if(n%4 == 3){
+    n++;
+  }
+  else{
+    n=0;
+  }
+  if(x & 1){
+    cout << x-n << "\n";
+    return;
+  }
+  else{
+    cout << x+n << "\n";
+    return;
+  } 
 }
-
-
-
 
 int main(){
   int t;
