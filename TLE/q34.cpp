ed81eda087ee1d2ce070c98bd9eb0fad84d68264
hackerAman <104302829+hackerAman1729@users.noreply.github.com>
2024-06-20 05:43:51 +0000

    TLE q34

--- TLE/q34.cpp
+++ TLE/q34.cpp
@@ -2,12 +2,28 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
 #define ll long long
 
 void solve(){
+  int n;
+  cin >> n;
+  vector<ll> v(n);
+  ll cnt1 = 0;
+  ll cnt0 = 0;
+  for(auto &i : v){
+    cin >> i;
+    if(i == 0){ 
+    cnt0++;}
+    if(i==1){
+      cnt1++;
+    }
+  }
+  
+  cout << (ll)pow(2, cnt0) * cnt1 << "\n";
 
   
 }
