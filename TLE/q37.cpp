49e96a45cfcb83e05421838995cebb842d4f6633
hackerAman <104302829+hackerAman1729@users.noreply.github.com>
2024-06-20 15:53:53 +0000

    q37 TLE

---
+++ TLE/q37.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+#define ll long long
+
+void solve(){
+  int n,m,i,j;
+  cin >> n>>m>>i>>j;
+  cout << 1 << " "<< 1<<" "<<n<<" "<<m<<"\n";
+
+}
+
+
+
+
+int main(){
+  int t;
+  cin>>t;
+  while (t--){
+    solve();
+  }
+  return 0;
+}
