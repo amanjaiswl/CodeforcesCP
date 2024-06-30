fad165ec75d8d6944136683b65fdce843c48ce50
Aman Jaiswal <104302829+amanjaiswl@users.noreply.github.com>
2024-06-30 05:11:20 +0000

    nm

--- 166Div2/q3.cpp
+++ 166Div2/q3.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
+#include <vector>
+#include <string>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
+#define ll long long
+
+void solve(){
+
+}
+
+
+
+
 int main(){
   int t;
-  cin >> t;
-  while(t--){
-
-
-
+  cin>>t;
+  while (t--){
+    solve();
   }
-
-
-
+  return 0;
 }
